32464efaae1fcdc49660638fb8487e1a1967f767
Coen Munckhof <c.munckhof@gmail.com>
2020-01-05 19:53:59 +0100

    Add some tests

--- src/AsyncExifTool/Logging/LogLevel.cs
+++ src/AsyncExifTool/Logging/LogLevel.cs
@@ -3,6 +3,11 @@
     public enum LogLevel
     {
         /// <summary>
+        /// For trace debugging; begin method X, end method X
+        /// </summary>
+        Trace,
+
+        /// <summary>
         /// For debugging.
         /// </summary>
         Debug,
@@ -13,17 +18,17 @@
         Info,
 
         /// <summary>
-        /// Something unexpected; application will continue.
+        /// Something unexpected; AsyncExifTool library will continue.
         /// </summary>
         Warn,
 
         /// <summary>
-        /// Something failed; application may or may not continue
+        /// Something failed; AsyncExifTool library may or may not continue
         /// </summary>
         Error,
 
         /// <summary>
-        /// Something bad happened; application is going down
+        /// Something bad happened; AsyncExifTool library is going down
         /// </summary>
         Fatal
     }
---
+++ tests/AsyncExifTool.Test/Logging/AllLogLevels.cs
@@ -0,0 +1,23 @@
+﻿namespace CoenM.ExifToolLibTest.Logging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using CoenM.ExifToolLib.Logging;
+    using Xunit;
+
+    internal class AllLogLevels : TheoryData<LogLevel>
+    {
+        private static readonly IEnumerable<LogLevel> AllItems = Enum
+                                                                 .GetValues(typeof(LogLevel))
+                                                                 .Cast<LogLevel>()
+                                                                 .AsEnumerable();
+
+        public AllLogLevels()
+        {
+            foreach (var logLevel in AllItems)
+                Add(logLevel);
+        }
+    }
+}
