eeea5df582e9034dd389712aef710f171ebbe395
Coen Munckhof <c.munckhof@gmail.com>
2020-01-05 20:44:07 +0100

    Revert "Revert "Add some tests""
    This reverts commit 6d6c6571b738b21c8c4e3c05ebbee5614de5c9f1.

--- src/AsyncExifTool/AsyncExifToolExtensions.cs
+++ src/AsyncExifTool/AsyncExifToolExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace CoenM.ExifToolLib
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -8,12 +9,12 @@
 
     public static class AsyncExifToolExtensions
     {
-        public static Task<string> GetVersionAsync(this AsyncExifTool @this, CancellationToken ct = default)
+        public static Task<string> GetVersionAsync([NotNull] this AsyncExifTool @this, CancellationToken ct = default)
         {
             return ExecuteAsync(@this, ExifToolArguments.Version, ct);
         }
 
-        public static Task<string> ExecuteAsync(this AsyncExifTool @this, string singleArg, CancellationToken ct = default)
+        public static Task<string> ExecuteAsync([NotNull] this AsyncExifTool @this, string singleArg, CancellationToken ct = default)
         {
             if (@this == null)
                 throw new ArgumentNullException(nameof(@this));
