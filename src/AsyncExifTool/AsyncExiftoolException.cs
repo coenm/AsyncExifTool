a5e9b6a0fcc33fade45b2685e0e7c0ed8a1b1c88
Coen Munckhof <c.munckhof@gmail.com>
2020-01-07 19:39:55 +0100

    Exception is serializable

--- src/AsyncExifTool/AsyncExiftoolException.cs
+++ src/AsyncExifTool/AsyncExiftoolException.cs
@@ -1,10 +1,18 @@
 ﻿namespace CoenM.ExifToolLib
 {
     using System;
+    using System.Runtime.Serialization;
+    using System.Security.Permissions;
 
     [Serializable]
-    public class AsyncExiftoolException : Exception
+    public sealed class AsyncExiftoolException : Exception
     {
+        private const int CurrentSerializationVersion = 1;
+        private const string VersionKey = "v";
+        private const string ExitCodeKey = "exit";
+        private const string StandardOutputKey = "stdout";
+        private const string StandardErrorKey = "stderr";
+
         public AsyncExiftoolException(int exitCode, string standardOutput, string standardError)
             : base(standardError)
         {
@@ -13,10 +21,39 @@
             StandardError = standardError;
         }
 
-        public int ExitCode { get; }
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        private AsyncExiftoolException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            var version = info.GetInt32(VersionKey);
 
-        public string StandardOutput { get; }
+            if (version != CurrentSerializationVersion)
+                throw new SerializationException("Could not deserialize data");
 
-        public string StandardError { get; }
+            ExitCode = info.GetInt32(ExitCodeKey);
+            StandardOutput = info.GetString(StandardOutputKey);
+            StandardError = info.GetString(StandardErrorKey);
+        }
+
+        public int ExitCode { get; private set; }
+
+        public string StandardOutput { get; private set; }
+
+        public string StandardError { get; private set; }
+
+
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new ArgumentNullException(nameof(info));
+
+            info.AddValue(VersionKey, CurrentSerializationVersion);
+            info.AddValue(ExitCodeKey, ExitCode);
+            info.AddValue(StandardOutputKey, StandardOutput);
+            info.AddValue(StandardErrorKey, StandardError);
+
+            base.GetObjectData(info, context);
+        }
     }
 }
