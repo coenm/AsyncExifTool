1d45cc9fb3ba78768801901b67d642dae608d306
Coen van den Munckhof <c.munckhof@gmail.com>
2020-05-08 10:45:01 +0200

    Update install script Exiftool using the new url (exiftool.org)

--- scripts/InstallExiftool.sh
+++ scripts/InstallExiftool.sh
@@ -9,7 +9,7 @@ ROOT_PATH=$(cd ${SCRIPTPATH}/../; pwd)
 EXIFTOOL_VERSION=$(cat "${ROOT_PATH}/EXIFTOOL_VERSION" | tr -d ' \n' | tr -d ' \r')
 echo "Exiftool version to install: ${EXIFTOOL_VERSION}"
 
-wget https://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-${EXIFTOOL_VERSION}.tar.gz
+wget http://exiftool.org/Image-ExifTool-${EXIFTOOL_VERSION}.tar.gz
 gzip -dc Image-ExifTool-${EXIFTOOL_VERSION}.tar.gz | tar -xf -
 cd Image-ExifTool-${EXIFTOOL_VERSION}
 perl Makefile.PL
